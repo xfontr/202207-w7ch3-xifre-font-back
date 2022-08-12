eef526493fdb690e34533bf5981717a92f5bcdc8
Xifre Font <xifre.font.97@gmail.com>
2022-08-12 20:30:01 +0200

    Set up debug and chalk packages

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,12 @@
 import express from "express";
+import Debug from "debug";
+
+const debug = Debug("robots:index");
 
 const app = express();
 
 const port = process.env.PORT ?? 4000;
 
 app.listen(port, () => {
-  console.log("Listening");
+  debug("hi");
 });
