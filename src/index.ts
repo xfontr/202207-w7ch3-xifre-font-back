25b42812de0b6109d1b0771dd3bd59a6218915a5
Pau <103753396+pau-develop@users.noreply.github.com>
2022-08-12 21:55:27 +0200

    Created and exported router

--- src/index.ts
+++ src/index.ts
@@ -2,12 +2,15 @@ import "./loadEnvironment";
 import express from "express";
 import Debug from "debug";
 import chalk from "chalk";
+import robotsRouter from "./server/routers/robotsRouter";
 
 const debug = Debug("robots:index");
 
 const app = express();
 
 const port = process.env.PORT ?? 4000;
+
+app.use("/robots", robotsRouter);
 
 app.listen(port, () => {
   debug(chalk.blue(`Server listening on ${port}`));
