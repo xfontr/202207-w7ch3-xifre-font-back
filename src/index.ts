a85f2d274e5307c2f80effca152e2677fbbde1d7
Xifre Font <xifre.font.97@gmail.com>
2022-08-12 22:55:16 +0200

    Added coverage exclusions at jest

--- src/index.ts
+++ src/index.ts
@@ -3,9 +3,13 @@ import { connectDB, startServer } from "./server/startServer";
 
 const port = +process.env.PORT ?? 4000;
 
-const mongoUrl = "";
+const mongoUrl = process.env.DATABASE;
 
 (async () => {
-  startServer(port);
-  await connectDB(mongoUrl);
+  try {
+    startServer(port);
+    await connectDB(mongoUrl);
+  } catch (error) {
+    process.exit(1);
+  }
 })();
