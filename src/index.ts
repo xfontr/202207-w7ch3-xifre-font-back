70f0e561ed1df2920a7c50565c9c67070d501487
Xifre Font <xifre.font.97@gmail.com>
2022-08-13 10:52:09 +0200

    Moved the app.use at the start server module

--- src/index.ts
+++ src/index.ts
@@ -1,22 +1,14 @@
 import "./loadEnvironment";
-import express from "express";
 import { connectDB, startServer } from "./server/startServer";
-import robotsRouter from "./server/routers/robotsRouter";
-
 
 const mongoUrl = process.env.DATABASE;
 
 const port = +process.env.PORT ?? 4000;
 
-const app = express();
-
-app.use("/robots", robotsRouter);
-
-
 (async () => {
   try {
-    startServer(port);
     await connectDB(mongoUrl);
+    startServer(port);
   } catch (error) {
     process.exit(1);
   }
--- src/server/controllers/robotControllers.ts
+++ src/server/controllers/robotControllers.ts
@@ -1,16 +1,14 @@
-import { Response } from "express";
+import { Request, Response } from "express";
 import Debug from "debug";
-import chalk from "chalk";
 import Robot from "../../database/models/Robots";
 
 const debug = Debug("robots:robotControllers");
 
-debug(chalk.green("test"));
+const getAllRobots = async (req: Request, res: Response) => {
+  const robots = await Robot.find({});
 
-const getAllRobots = async (res: Response) => {
-  const robots = await Robot.find();
-  debug(robots);
-  res.status(200).json({ robots });
+  await debug(robots);
+  await res.status(200).json(robots);
 };
 
 export default getAllRobots;
