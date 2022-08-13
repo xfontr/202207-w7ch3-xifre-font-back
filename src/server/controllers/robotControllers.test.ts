b23179cafc1ac6d32a8981ff24c084a565e7022b
Xifre Font <xifre.font.97@gmail.com>
2022-08-13 12:42:16 +0200

    Tested that json returns specific robot by params

--- src/server/controllers/robotControllers.test.ts
+++ src/server/controllers/robotControllers.test.ts
@@ -22,5 +22,39 @@ describe("Given a function getRobot", () => {
 
       expect(res.status).toBeCalledWith(status);
     });
+
+    test("And it should invoke the 'json' method and return one specific robot", async () => {
+      const req = {
+        params: { idRobot: "1" } as any,
+      };
+
+      const res = {
+        status: jest.fn().mockReturnThis(),
+        json: jest.fn(),
+      } as Partial<Response>;
+
+      const next = jest.fn() as Partial<NextFunction>;
+
+      const mockRobots = [
+        {
+          id: "1",
+          name: "",
+          img: "",
+          creationDate: "",
+          speed: 0,
+          endurance: 0,
+        },
+      ];
+
+      const { idRobot } = req.params;
+
+      Robot.find = jest
+        .fn()
+        .mockReturnValue(mockRobots.filter((robot) => robot.id === idRobot));
+
+      await getRobot(req as Request, res as Response, next as NextFunction);
+
+      expect(res.json).toHaveBeenCalledWith([mockRobots[0]]);
+    });
   });
 });
